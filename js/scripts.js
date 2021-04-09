b5ac3b082ba5f6b1015c60131fb9d3389e5ab30e
Thomas Russell <t.p.russell19@gmail.com>
2021-04-09 09:15:03 -0700

    add passing code for Pizza constructor

---
+++ js/scripts.js
@@ -0,0 +1,8 @@
+function Pizza(size, topping1, topping2, topping3) {
+  this.size = size;
+  this.topping1 = topping1;
+  this.topping2 = topping2;
+  this.topping3 = topping3;
+}
+
+let newPizza = new Pizza("large", "pepperoni", "red-onions", "grenn-peppers");
