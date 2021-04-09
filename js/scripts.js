26b8f2ca684998e249e74bafb3704ddd02abe85f
Thomas Russell <t.p.russell19@gmail.com>
2021-04-09 16:51:06 -0700

    add showing/hiding the finished order section and its contents

--- js/scripts.js
+++ js/scripts.js
@@ -7,13 +7,13 @@ function Pizza(size, topping1, topping2, topping3) {
 }
 
 Pizza.prototype.pizzaPrice = function() {
-  if (this.size === "personal") {
+  if (this.size === "Personal") {
     this.price += 10;
-  } else if (this.size === "medium") {
+  } else if (this.size === "Medium") {
     this.price += 15;
-  } else if (this.size === "large") {
+  } else if (this.size === "Large") {
     this.price += 20;
-  } else if (this.size === "extra-large") {
+  } else if (this.size === "Extra-large") {
     this.price += 25;
   }
   if (this.topping1 === 0) {
@@ -37,9 +37,26 @@ Pizza.prototype.pizzaPrice = function() {
 $(document).ready(function() {
   $("form").submit(function(event) {
     event.preventDefault();
-    let newPizza = new Pizza($("#size").val(), parseInt($("#topping-1").val()), parseInt($("#topping-2").val()), parseInt($("#topping-3").val()));
-    console.log(newPizza);
-    newPizza.pizzaPrice();
-    $("#new-pizza").text(newPizza.price);
+    let orderPrice = new Pizza($("#size").val(), parseInt($("#topping-1").val()), parseInt($("#topping-2").val()), parseInt($("#topping-3").val()));
+    let pizzaOrder = new Pizza($("#size").val(), $("#topping-1").val(), $("#topping-2").val(), $("#topping-3").val());
+    console.log(orderPrice, pizzaOrder);
+    orderPrice.pizzaPrice();
+    if (orderPrice.size === "select") {
+      $("#select-a-size").show();
+      $("#completed-order").hide();
+    } else {
+      $("#completed-order").show();
+      $("#order-size").text(`Size: ${pizzaOrder.size}`);
+      if (pizzaOrder.topping1 !== "0") {
+        $("#order-topping-1").text(`Topping #1: ${pizzaOrder.topping1}`);
+      }
+      if (pizzaOrder.topping2 !== "0") {
+        $("#order-topping-2").text(`Topping #2: ${pizzaOrder.topping2}`);
+      }
+      if (pizzaOrder.topping3 !== "0") {
+        $("#order-topping-3").text(`Topping #3: ${pizzaOrder.topping3}`);
+      }
+      $("#new-pizza").text(`Your Total: $${orderPrice.price}`)
+    }
   })
 })
