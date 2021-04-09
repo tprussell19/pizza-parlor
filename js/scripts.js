5b57c8ededcd430493d3dc4d566f8845046b298e
Thomas Russell <t.p.russell19@gmail.com>
2021-04-09 09:16:43 -0700

    add property for price in Pizza constructor

--- js/scripts.js
+++ js/scripts.js
@@ -3,6 +3,7 @@ function Pizza(size, topping1, topping2, topping3) {
   this.topping1 = topping1;
   this.topping2 = topping2;
   this.topping3 = topping3;
+  this.price = 0;
 }
 
 let newPizza = new Pizza("large", "pepperoni", "red-onions", "grenn-peppers");
